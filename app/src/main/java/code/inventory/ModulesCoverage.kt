20f4247e0662fa252f3ca592212e3d23c9354fad
RishabhDuttSharma <a_rishabh.sharma@airtel.com>
2018-04-30 19:17:06 +0530

    Adding PermissionManager

---
+++ app/src/main/java/code/inventory/ModulesCoverage.kt
@@ -0,0 +1,19 @@
+package code.inventory
+
+/**
+ * Developer: Rishabh Dutt Sharma
+ * Dated: 4/30/2018.
+ */
+enum class ModulesCoverage(val completed: Boolean) {
+
+    NETWORK(true),
+    DATABASE(true),
+    AUTO_LOG(true),
+    VALIDATION(true), // 30-Apr-2018
+    LOCATION(false),
+    UNZIPPER(false),
+    APP_UPDATER(false),
+    SMS_READER(false),
+    PERMISSION_MANAGER(false),
+    ENCRYPTION_MANAGER(false),
+}
