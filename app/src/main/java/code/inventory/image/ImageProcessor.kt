1b2102a321d84c864ea2360845ac34a1c82bab85
RishabhDuttSharma <a_rishabh.sharma@airtel.com>
2018-05-11 18:25:50 +0530

    Documentation for ImageProcessor

--- app/src/main/java/code/inventory/image/ImageProcessor.kt
+++ app/src/main/java/code/inventory/image/ImageProcessor.kt
@@ -3,10 +3,19 @@ package code.inventory.image
 import android.graphics.Bitmap
 
 /**
+ * Standard interface for processing Images (i.e., bitmaps)
+ *
  * Developer: Rishabh Dutt Sharma
  * Dated: 11-May-18.
  */
 interface ImageProcessor {
 
+    /**
+     * Processes the Image specified by BitmapProvider
+     *
+     * @param bitmapProvider provider for Bitmap
+     *
+     * @return instance of processed Bitmap
+     */
     fun process(bitmapProvider: BitmapProvider): Bitmap
 }
