d1d7513bd88127abe0c6fcaf0caf09f9e8a7e88d
RishabhDuttSharma <a_rishabh.sharma@airtel.com>
2018-05-17 11:14:51 +0530

    Documentation for ImageProcessorHelper

--- app/src/main/java/code/inventory/image/ImageProcessorHelper.kt
+++ app/src/main/java/code/inventory/image/ImageProcessorHelper.kt
@@ -9,6 +9,8 @@ import java.util.concurrent.LinkedBlockingQueue
 import kotlin.concurrent.thread
 
 /**
+ * Helper class to perform multiple ImageProcessing on an Image-Bitmap
+ *
  * Developer: Rishabh Dutt Sharma
  * Dated: 16-May-18.
  */
@@ -16,7 +18,9 @@ class ImageProcessorHelper private constructor(private val bitmap: Bitmap,
                                                private val imageProcessingQueue: LinkedBlockingQueue<ImageProcessor>) {
 
     /**
-     * Executes
+     * Executes the Image Processing Queue asynchronously
+     *
+     * @param listener an instance of listener that would receive processing results
      */
     @Throws(Exception::class)
     fun executeAsync(listener: OnCompletedListener?) {
@@ -32,6 +36,11 @@ class ImageProcessorHelper private constructor(private val bitmap: Bitmap,
         }
     }
 
+    /**
+     * Executes the ImageProcessors on same thread
+     *
+     * @return processed image-bitmap
+     */
     fun execute(): Bitmap {
         var processedBitmap: Bitmap = bitmap
         imageProcessingQueue.forEach { imageProcessor ->
@@ -40,26 +49,69 @@ class ImageProcessorHelper private constructor(private val bitmap: Bitmap,
         return processedBitmap
     }
 
+    /**
+     * Builds an instance of ImageProcessorHelper
+     */
     class Builder(private val bitmap: Bitmap?) {
 
         private val imageProcessingQueue = LinkedBlockingQueue<ImageProcessor>()
 
+        /**
+         * Resize the Bitmap to given width and height
+         *
+         * @param width target width of bitmap
+         * @param height target height of the bitmap
+         */
         fun resize(width: Int, height: Int) = also { imageProcessingQueue.offer(ImageResizer(width, height)) }
 
+        /**
+         * Downgrades the quality of the Bitmap to given quality value
+         *
+         * @param quality target quality of bitmap
+         */
         fun compress(quality: Int) = also { imageProcessingQueue.offer(ImageCompressor(quality)) }
 
+        /**
+         * Crops the bitmap with given parameters
+         *
+         * @param x column pixel of start of crop
+         * @param y row pixel of start of crop
+         * @param width width of the crop
+         * @param height height of the crop
+         */
         fun crop(x: Int, y: Int, width: Int, height: Int) = also { imageProcessingQueue.offer(ImageCropper(x, y, width, height)) }
 
+        /**
+         * Rotates the bitmap to given angle
+         *
+         * @param angle the angle at the which the bitmap should be rotated
+         */
         fun rotate(angle: Float) = also { imageProcessingQueue.offer(ImageRotator(angle)) }
 
+        /**
+         * @return an instance of ImageProcessorHelper as specified by Builder Parameters
+         */
         fun build() = ImageProcessorHelper(bitmap
                 ?: throw Exception("Image Helper: build -> bitmap is null"), imageProcessingQueue)
     }
 
+    /**
+     * Interface callback definition, invoked when the ImageProcessorHelper has
+     * completed processing the Bitmap.
+     */
     interface OnCompletedListener {
 
+        /**
+         * Called when the Bitmap has been successfully processed
+         * through given queue of ImageProcessors
+         *
+         * @param bitmap the final processed bitmap
+         */
         fun onProcessed(bitmap: Bitmap)
 
+        /**
+         * Called when an Error is occurred while processing the Bitmap
+         */
         fun onError(throwable: Throwable)
     }
 }
